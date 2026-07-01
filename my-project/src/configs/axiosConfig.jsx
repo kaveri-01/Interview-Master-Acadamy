2c375a24c168329e08ba55240cdd69be447024d7
kaveri koli <112814588+kaveri-01@users.noreply.github.com>
2026-07-01 14:06:31 +0530

    BE url added

--- my-project/src/configs/axiosConfig.jsx
+++ my-project/src/configs/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://rujal-x-adarsh-traders-backend.onrender.com/api/v1",
-  baseURL: "http://localhost:8000/api/v1", 
+  baseURL: "https://ima-backend.onrender.com/api/v1",
+  // baseURL: "http://localhost:8000/api/v1", 
   headers: {
     "Content-Type": "application/json",
   },
